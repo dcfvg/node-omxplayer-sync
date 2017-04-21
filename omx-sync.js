443d2b514823bba8cd9d5934c9806e4d4a8e05ff
dcfvg <qwsx@dcfvg.com>
2017-04-21 08:21:35 +0200

    trigger loop on new client

--- omx-sync.js
+++ omx-sync.js
@@ -1,7 +1,6 @@
 var fs = require('fs');
 var server = require('http').createServer();
-var socket = require('socket.io-client')('http://192.168.0.99:3000');
-// var io = require('socket.io')(server);
+var socket = require('socket.io-client')('http://192.168.1.11:3000');
 var io = require('socket.io')(server, {'pingInterval': 10000, 'pingTimeout': 15000});
 var dbus = require('dbus-native');
 var exec = require('child_process').exec;
@@ -16,9 +15,10 @@ server.listen(3000, function() { console.log( 'Listening on port 3000') });
 
 // PARSE TERMINAL INPUT.
 if(file == undefined){
-  console.log("no video file specified");
+  console.log('no video file specified');
   return
 }
+console.log('current video path: ' + file);
 
 //kill previous player if the script needs to restart
 var killall = exec('killall omxplayer.bin', (error, stdout, stderr) => {
@@ -29,8 +29,6 @@ var killall = exec('killall omxplayer.bin', (error, stdout, stderr) => {
   console.log(`stdout: ${stdout}`);
   console.log(`stderr: ${stderr}`);
 });
-
-console.log('current video path: ' + file);
 
 //start omx player
 omx = exec('omxplayer '+options+file, (error, stdout, stderr) => {
@@ -46,88 +44,88 @@ omx.on('exit', function(code){
   console.log('EXIT CODE: '+ code +' @ ' + Date() );
   // relaunch omxplayer
   process.exit(0);
-
 });
 
 //SOCKET.IO HANDLING
 io.on('connection', function(socket){
-  console.log("Listener connected: " + socket.id + ' @ ' + Date());
+
+  var address = socket.handshake.address;
+  console.log('Listener '+ address.address+' connected: ' + socket.id + ' @ ' + Date());
+
+  // fire loop flag on client connection
+  setTimeout(function(){
+    io.emit('loopFlag', { loopFlag : 'loop' });
+  }, 4000);
 
   socket.on('disconnect', function(){
-    console.log("Listener disconnected: " + socket.id );
+    console.log('Listener '+ address.address+' disconnected: ' + socket.id + ' @ ' + Date());
   });
 
 });
 
 socket.on('connect', function(){
-  console.log("Connected to the broadcaster as: " + socket.id + ' @ ' + Date() );
-
+  console.log('Connected to the broadcaster as: ' + socket.id + ' @ ' + Date() );
 });
 
 socket.on('loopFlag', function(loopFlag){
   console.log('loop flag recieved  @ ' + Date());
   seek( s2micro(1) );
-  setTimeout(function(){
-    gate = true;
-  },1000)
+  setTimeout(function(){ gate = true },1000)
 })
 
 //DBUS HANDLING
 setTimeout(function(){ //wait for dbus to become available.
 
   bus = dbus.sessionBus({
-          busAddress: fs.readFileSync('/tmp/omxplayerdbus.pi', 'ascii').trim()
+    busAddress: fs.readFileSync('/tmp/omxplayerdbus.pi', 'ascii').trim()
   });
 
   setTimeout(function(){
     bus.invoke({
-            path: "/org/mpris/MediaPlayer2",
-            interface: "org.freedesktop.DBus.Properties",
-            member: "Duration",
-            destination: "org.mpris.MediaPlayer2.omxplayer",
+      path: '/org/mpris/MediaPlayer2',
+      interface: 'org.freedesktop.DBus.Properties',
+      member: 'Duration',
+      destination: 'org.mpris.MediaPlayer2.omxplayer',
     }, function(err, duration) {
-            totalDuration = duration; //set to a global
-            console.log("Duration: " + totalDuration);
+      totalDuration = duration; //set to a global
+      console.log('Duration: ' + totalDuration);
     });
 
     //send out loop flag
     setInterval(function(){
       bus.invoke({
-              path: "/org/mpris/MediaPlayer2",
-              interface: "org.freedesktop.DBus.Properties",
-              member: "Position",
-              destination: "org.mpris.MediaPlayer2.omxplayer",
+        path: '/org/mpris/MediaPlayer2',
+        interface: 'org.freedesktop.DBus.Properties',
+        member: 'Position',
+        destination: 'org.mpris.MediaPlayer2.omxplayer',
       }, function(err, position) {
               currentPosition = position; //set to a global
-              // console.log("CP: " + currentPosition);
+              // console.log('CP: ' + currentPosition);
       });
-
 
       if(currentPosition >= totalDuration - s2micro(1) && gate == true){ //are we in the end range of the file?
         gate = false;
-        console.log( "*File Ended @ " + Date() );
+        console.log( '*File Ended @ ' + Date() );
         io.emit('loopFlag', { loopFlag : 'loop' }); //add one of these above outside the interval loop to reset when the server boots?
-      }
+      };
 
     },250);
 
-  }, 1000)
+  }, 1000);
+
 }, 1000);
 
 function seek(pos){
   bus.invoke({
-          path: "/org/mpris/MediaPlayer2",
-          interface: "org.mpris.MediaPlayer2.Player",
-          member: "SetPosition",
-          destination: "org.mpris.MediaPlayer2.omxplayer",
-          signature: "ox",
+          path: '/org/mpris/MediaPlayer2',
+          interface: 'org.mpris.MediaPlayer2.Player',
+          member: 'SetPosition',
+          destination: 'org.mpris.MediaPlayer2.omxplayer',
+          signature: 'ox',
           body: [ '/not/used', pos ]
   }, function(err) {
-    if(err != null){
-          console.log("ERROR: "+err);
-        }
+    if(err != null) console.log('ERROR: '+err);
   });
-
 }
 
 function s2micro(seconds){
